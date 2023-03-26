21a1323a5d5f1fe074857bfe37d5a92ec12fa863
castlebbs <david@ombrepixel.com>
2023-03-26 19:44:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+data "aws-vpc" "default" {
+  default = true
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
