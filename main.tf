755536e05546570a0a58a19dba71c5e15be6eef9
castlebbs <david@ombrepixel.com>
2023-03-26 20:21:04 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws-vpc" "default" {
+data "aws_vpc" "default" {
   default = true
 }
 
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
   
-  vpc_id      = data.aws-vpc.default.id
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
